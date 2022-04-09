d6b25c6a268b77c31cd37e2da47b127ab417dda9
ThuongNguyen <thuongnguyen@ThuongNguyen.local>
2022-04-09 16:04:16 +0700

    AnalogueClock

---
+++ AnalogueClock/script.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius,radius);
+radius = radius * 0.90;
+
+drawColock();
+
+function drawColock(){
+    ctx.arc(0,0,radius,0,2* Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+}
